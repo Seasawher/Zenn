573659bff569eac7cd4122de4ba181aed25112fc
Seasawher <tellagoodstory@icloud.com>
2026-01-01 03:09:10 +0900

    Lean プロジェクトとして初期化する

---
+++ articles/Basic.md
@@ -0,0 +1,8 @@
+---
+title: "Zenn CLI のテスト"
+emoji: "😸"
+type: "tech"
+topics: ["Lean", "Lean4", "形式証明"]
+published: false
+---
+ここから本文を書く
---
+++ lakefile.lean
@@ -0,0 +1,34 @@
+import Lake
+
+open System Lake DSL
+
+package Articles where
+
+@[default_target]
+lean_lib Article where
+  globs := #[.submodules `Article]
+
+require mdgen from git
+  "https://github.com/Seasawher/mdgen" @ "main"
+
+def runCmdWithOutput (input : String) (stdIn : Option String := none) : IO String := do
+  let cmdList := input.splitOn " "
+  let cmd := cmdList.head!
+  let args := cmdList.tail |>.toArray
+  let out ← IO.Process.output
+    (args := {cmd := cmd, args := args})
+    (input? := stdIn)
+  unless out.exitCode == 0 do
+    IO.eprintln out.stderr
+    throw <| IO.userError s!"Failed to execute: {input}"
+
+  return out.stdout.trimAscii.copy
+
+def runCmd (input : String) : IO Unit := do
+  let out ← runCmdWithOutput input
+  if out != "" then
+    IO.println out
+
+script "build" do
+  runCmd "lake exe mdgen Article articles"
+  return 0
